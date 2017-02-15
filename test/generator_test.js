35b26e8e0b9b5253af1a37b1e89eb926078347ff
Rodrigo Alfonso <rodri042@gmail.com>
2017-02-15 01:10:14 -0300

    Cambio sintaxis de comentarios para que ande en gw

--- test/generator_test.js
+++ test/generator_test.js
@@ -40,7 +40,7 @@ suite('gs-element-blockly_generator', function() {
 
   gsTestCode('Procedimiento',
   '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="procedures_defnoreturn"><mutation><arg name="valor1"></arg><arg name="otroValor"></arg></mutation><field name="NAME">hacer algo con parametros</field><comment pinned="false" h="80" w="160">Un comentario para el procedimiento</comment></block></xml>',
-  `/**
+  `/*
  * Un comentario para el procedimiento
  */
 procedure HacerAlgoConParametros(valor1, otroValor) {
