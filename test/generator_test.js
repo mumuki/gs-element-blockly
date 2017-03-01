25fbd77e6c37b145e444b32e71679a2fb10fdefb
Alf Sanzo <alfredo.sanzo@gmail.com>
2017-03-01 16:23:25 -0300

    Haciendo que los tests se adecúen a los nuevos bloques con dropdown. Closes #28

--- test/generator_test.js
+++ test/generator_test.js
@@ -15,19 +15,19 @@ suite('gs-element-blockly_generator', function() {
     'program {\n}');
 
   gsTestCode('Poner',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Poner"><value name="COLOR"><block type="Rojo"></block></value></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Poner"><value name="COLOR"><block type="ColorSelector"><field name="ColorDropdown">Rojo</field></block></value></block></xml>',
     `Poner(Rojo)\n`);
 
   gsTestCode('Sacar',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Sacar"><value name="COLOR"><block type="Verde"></block></value></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Sacar"><value name="COLOR"><block type="ColorSelector"><field name="ColorDropdown">Verde</field></block></value></block></xml>',
     `Sacar(Verde)\n`);
 
   gsTestCode('Mover',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Mover"><value name="DIRECCION"><block type="Oeste"></block></value></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Mover"><value name="DIRECCION"><block type="DireccionSelector"><field name="DireccionDropdown">Oeste</field></block></value></block></xml>',
     `Mover(Oeste)\n`);
 
   gsTestCode('IrAlBorde',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="IrAlBorde"><value name="DIRECCION"><block type="Norte"></block></value></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="IrAlBorde"><value name="DIRECCION"><block type="DireccionSelector"><field name="DireccionDropdown">Norte</field></block></value></block></xml>',
     `IrAlBorde(Norte)\n`);
 
   gsTestCode('VaciarTablero',
@@ -47,7 +47,7 @@ procedure HacerAlgoConParametros(valor1, otroValor) {
 }\n`);
 
   gsTestCode('SiEntonces',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="SiEntonces"><value name="condicion"><block type="True"></block></value><statement name="block"><block type="Poner"><value name="COLOR"><block type="Rojo"></block></value></block></statement></block></statement></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="SiEntonces"><value name="condicion"><block type="BoolSelector"><field name="BoolDropdown">True</field></block></value><statement name="block"><block type="Poner"><value name="COLOR"><block type="ColorSelector"><field name="ColorDropdown">Rojo</field></block></value></block></statement></block></statement></block></xml>',
     `program {
   if (True) {
     Poner(Rojo)
@@ -55,7 +55,7 @@ procedure HacerAlgoConParametros(valor1, otroValor) {
 }`);
 
   gsTestCode('SiEntoncesSiNo',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="SiEntoncesSiNo"><value name="condicion"><block type="True"></block></value><statement name="block1"><block type="Poner"><value name="COLOR"><block type="Rojo"></block></value></block></statement><statement name="block2"><block type="Sacar"><value name="COLOR"><block type="Verde"></block></value></block></statement></block></statement></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="SiEntoncesSiNo"><value name="condicion"><block type="BoolSelector"><field name="BoolDropdown">True</field></block></value><statement name="block1"><block type="Poner"><value name="COLOR"><block type="ColorSelector"><field name="ColorDropdown">Rojo</field></block></value></block></statement><statement name="block2"><block type="Sacar"><value name="COLOR"><block type="ColorSelector"><field name="ColorDropdown">Verde</field></block></value></block></statement></block></statement></block></xml>',
     `program {
   if (True) {
     Poner(Rojo)
@@ -66,7 +66,7 @@ procedure HacerAlgoConParametros(valor1, otroValor) {
 }`);
 
   gsTestCode('Repetir',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="Repetir"><value name="count"><block type="math_number"><field name="NUM">5</field></block></value><statement name="block"><block type="Mover"><value name="DIRECCION"><block type="Este" id="wDQeQ%pLbF2m`O~hru+h"></block></value></block></statement></block></statement></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="Repetir"><value name="count"><block type="math_number"><field name="NUM">5</field></block></value><statement name="block"><block type="Mover"><value name="DIRECCION"><block type="DireccionSelector"><field name="DireccionDropdown">Este</field></block></value></block></statement></block></statement></block></xml>',
     `program {
   repeat(5) {
     Mover(Este)
@@ -74,7 +74,7 @@ procedure HacerAlgoConParametros(valor1, otroValor) {
 }`);
 
   gsTestCode('Llamada a procedimiento',
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false"><statement name="program"><block type="procedures_callnoreturn"><mutation name="hacer algo con"><arg name="x"></arg><arg name="y"></arg></mutation><value name="ARG0"><block type="Rojo"></block></value><value name="ARG1"><block type="Verde"></block></value></block></statement></block><block type="procedures_defnoreturn"><mutation><arg name="x"></arg><arg name="y"></arg></mutation><field name="NAME">hacer algo con</field><comment></comment></block></xml>',
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false"><statement name="program"><block type="procedures_callnoreturn"><mutation name="hacer algo con"><arg name="x"></arg><arg name="y"></arg></mutation><value name="ARG0"><block type="ColorSelector"><field name="ColorDropdown">Rojo</field></block></value><value name="ARG1"><block type="ColorSelector"><field name="ColorDropdown">Verde</field></block></value></block></statement></block><block type="procedures_defnoreturn"><mutation><arg name="x"></arg><arg name="y"></arg></mutation><field name="NAME">hacer algo con</field><comment></comment></block></xml>',
     `procedure HacerAlgoCon(x, y) {
 }
 
@@ -86,7 +86,7 @@ program {
 test('Procedimiento primitivo', function() {
   let element = document.getElementById("gseb");
   element.primitiveProcedures = ['Poner_FloresAl_'];
-  element.workspaceXml = `<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="Poner_FloresAl_"><value name="arg1"><block type="math_number"><field name="NUM">4</field></block></value><value name="arg2"><block type="Este"></block></value></block></statement></block></xml>`;
+  element.workspaceXml = `<xml xmlns="http://www.w3.org/1999/xhtml"><block type="Program" deletable="false" movable="false" editable="false" x="30" y="30"><statement name="program"><block type="Poner_FloresAl_"><value name="arg1"><block type="math_number"><field name="NUM">4</field></block></value><value name="arg2"><block type="DireccionSelector"><field name="DireccionDropdown">Este</field></block></value></block></statement></block></xml>`;
   assert.equal(element.generateCode(), `program {
   Poner_FloresAl_(4, Este)
 }`);
