30ec70c4be5ebf7c6082aeef304695c1ab5d35c3
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2017-12-11 17:31:26 -0300

    Agregando tipos de error #79

--- js/errors.js
+++ js/errors.js
@@ -10,20 +10,47 @@ Blockly.ErrorInforming.addToWorkspace = function(workspace){
     this.addCssToDocument(Blockly.ErrorInforming.CssContent.join('\n'));
 }
 
+// Kinds of errors:
+
+Blockly.ErrorInforming.GENERIC_ERROR = {
+    title: 'Problema',
+    cssClass: 'blocklyGenericError',
+};
+
+Blockly.ErrorInforming.INCOMPLETE_ERROR = {
+    title: 'Incompleto',
+    description: 'Falta completar aquí',
+    cssClass: 'blocklyIncompleteError',
+};
+
+Blockly.ErrorInforming.TYPE_ERROR = {
+    title: 'Problema de tipos',
+    cssClass: 'blocklyTypeError',
+};
+
+// Most languages have primitives / commands whose high-level logic has preconditions
+// to execute. For example, if moving thorugh a path, command "move" executed in a border
+// could throw an error. It is one possible kind of runtime error.
+Blockly.ErrorInforming.PRECONDITION_ERROR = {
+    title: 'Error',
+    cssClass: 'blocklyPreconditionError',
+};
+
+
 /**
  * Method to be mixed into a Blockly Workspace object.
  * Adds posibility of showing different types of errors to a block.
  */
-Blockly.ErrorInforming.showBlockError = function(blockId, description) {
+Blockly.ErrorInforming.showBlockError = function(blockId, kind = Blockly.ErrorInforming.GENERIC_ERROR) {
     let block = this.getBlockById(blockId);
-    Blockly.utils.addClass(block.getSvgRoot(),'blocklyErrorBlock');
-    block.setWarningText(description);
+    Blockly.utils.addClass(block.getSvgRoot(), kind.cssClass);
+    block.setWarningText(kind.title + (kind.description ? ': ' + kind.description : ''));
     block.warning.setVisible(true);
 };
 
 /**
  * Method to be mixed into a Blockly Workspace object.
- * Adds posibility of showing different types of errors to a block.
+ * Removes all errors present in the workspace
  */
 Blockly.ErrorInforming.removeBlockErrors = function(){
     this.getAllBlocks().filter(block => block.warning).map(function(block){
@@ -42,8 +69,23 @@ Blockly.ErrorInforming.addCssToDocument = function (cssText) {
 }
 
 Blockly.ErrorInforming.CssContent = [
-    '.blocklyErrorBlock>.blocklyPath {',
+    '.blocklyGenericError>.blocklyPath {',
+        'stroke: #f60;',
+        'stroke-width: 3px;',
+    '}',
+
+    '.blocklyIncompleteError>.blocklyPath {',
         'stroke: #f00;',
-        'stroke-width: 5px;',
+        'stroke-width: 3px;',
+    '}',
+    
+    '.blocklyTypeError>.blocklyPath {',
+        'stroke: #f00;',
+        'stroke-width: 3px;',
+    '}',
+
+    '.blocklyPreconditionError>.blocklyPath {',
+        'stroke: #f00;',
+        'stroke-width: 3px;',
     '}',
 ];
