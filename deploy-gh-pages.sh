e6aeb52c13f106d7f1364158c1eceb2cbd21bb59
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2017-12-27 19:42:15 -0300

    Arreglo scripts deploy agregando info branch

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,5 +1,7 @@
+branch=${3:-"dev"} # default to dev when branch isn't specified
+
 mkdir tempGH
 cd tempGH
-../gp.sh Program-AR gs-element-blockly dev
+../gp.sh Program-AR gs-element-blockly ${branch}
 cd ..
 rm -rf tempGH
