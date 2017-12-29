59bc80946be4ce4a3d53018a6123a8a8d7205e71
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2017-12-29 11:17:14 -0300

    Refactoring debugging code for pragma tests

--- test/helper.js
+++ test/helper.js
@@ -4,7 +4,7 @@
 function gsTestCode(name, xml, code, options) {
 	test(name, function() {
 		this.element.workspaceXml = xml;
-		printPragmaRow(this.element,options,name);
+		printPragmaRow(this.element,options,name); //For debugging pragma printing results
 		assert.equal(this.element.generateCode(options), code);
 	});
 }
@@ -22,7 +22,7 @@ function gsSuite(name,func) {
 	});
 }
 
-function printPragmaRow(element,options,name){
+function printPragmaRow(element,options,name){ // Uncomment table in test/index.html for this to work
 	var out = document.getElementById("testOutput");
-	if(options) out.innerHTML = out.innerHTML + `<tr><td>${name}</td><td>${element.generateCode()}</td><td>${element.generateCode(options)}</td></tr>`.replace(/\n/g,"<br/>").replace(/  /g,"&nbsp;&nbsp;");
+	if(out && options) out.innerHTML = out.innerHTML + `<tr><td>${name}</td><td>${element.generateCode()}</td><td>${element.generateCode(options)}</td></tr>`.replace(/\n/g,"<br/>").replace(/  /g,"&nbsp;&nbsp;");
 }
