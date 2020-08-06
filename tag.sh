7f7f7d0b22bf18a27a3b65aad135186812adcb7e
Federico Aloi <federico.aloi@gmail.com>
2020-08-06 11:30:49 -0300

    Tag script increases both bower and npm version

--- tag.sh
+++ tag.sh
@@ -13,13 +13,14 @@ fi
 
 echo "[Gobstones::Blockly] Updating version..."
 sed -i -r "s/\"version\": \"${VERSION_REGEXP}/\"version\": \"${NEW_VERSION}/" bower.json
+sed -i -r "s/\"version\": \"${VERSION_REGEXP}/\"version\": \"${NEW_VERSION}/" package.json
 sed -i -r "s/VERSION = \"${VERSION_REGEXP}/VERSION = \"${NEW_VERSION}/" gem/lib/gobstones/blockly/version.rb
 
 echo "[Gobstones::Blockly] Testing if the build is ok..."
 ./build.sh
 
 echo "[Gobstones::Blockly] Commiting files..."
-git commit bower.json gem/lib/gobstones/blockly/version.rb -m "Welcome ${NEW_VERSION}!"
+git commit bower.json package.json gem/lib/gobstones/blockly/version.rb -m "Welcome ${NEW_VERSION}!"
 
 echo "[Gobstones::Blockly] Tagging $NEW_VERSION..."
 git tag "${NEW_VERSION}"
